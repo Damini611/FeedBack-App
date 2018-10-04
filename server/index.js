387b66511bd649a538790ea842dc788bfc0e607e
Damini Singh <damini.singh@mavs.uta.edu>
2018-10-03 22:02:20 -0400

    Changes to first app

--- server/index.js
+++ server/index.js
@@ -3,6 +3,6 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello World this is the first Express App!'));
 
 app.listen(5000);
