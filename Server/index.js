a205711fa2bd78c631fc2354d77b54bb79c540f2
Damini Singh <damini.singh@mavs.uta.edu>
2018-10-11 20:21:02 -0400

    Change_11Oct

--- Server/index.js
+++ Server/index.js
@@ -1,8 +1,48 @@
 //https://stackoverflow.com/questions/46677752/the-difference-between-requirex-and-import-x/46677972
 const express = require('express');
-
 const app = express();
-var port = process.env.PORT || 8080;
-app.get('/', (req, res) => res.send('Hello World this is the first Express App!'));
+const passport = require('passport');
+//We only want to import strategy properrt of passport-google-oauth20 module
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+var port = process.env.PORT || 5000;
+var test = require('./config/keys');
 
+//This is route handler that is looking for a specific incoming HTTP requests.
+//Express app has other methods too: get, put, post, delete , patch
+//'/' --> specifies the Route: This App wastches for the requests trying to access '/'
+// req - Object representing incoming request
+// res - Object representing the outgoing response
+//res.send: A JSON is rendered on the client browser which made this request '/'
+app.get('/', (req, res) => res.send("Hello World !!"));
+//https://console.developers.google.com/?pli=1
+
+//Passing a GoogleStrategy constructor to use method
+//callback URL is the redirect URI which is registered at console.developer.google.com and users get
+// redirected to this page after google grant the permission to user
+passport.use(new GoogleStrategy(
+  {
+  clientID: test.Googleclientid,
+  clientSecret: test.Googlesecretclientid,
+  callbackURL: "/auth/google/callback"
+},
+   (accessToken, refreshToken, profile, done) => {
+    console.log(accessToken);
+    console.log("refresh " + refreshToken);
+    console.log("profile" + JSON.stringify(profile));
+  }
+ )
+);
+
+// when we use the string 'google' , we are internally using the above defined  GoogleStrategy object
+//Google has a list of various other scopes as well like read all contact images, photos inside drive
+app.get('/auth/google', passport.authenticate('google', { scope: ['profile','email'] }));
+
+//When user is redirected to the path "/auth/google/callback", there will be a "CODE" in the URL which is sent by Google server
+//passport will not try to authenticate the user for the first time but instead share the code with the google servers
+app.get('/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.send("Successful");
+  });
 app.listen(port);
