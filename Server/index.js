a69822b521d30473e9668ebd2f28139241039faa
Damini Singh <damini.singh@mavs.uta.edu>
2019-03-02 18:10:40 -0500

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -5,7 +5,7 @@ const passport = require('passport');
 //We only want to import strategy property of passport-google-oauth20 module
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 var port = process.env.PORT || 5000;
-var test = require('./config/keys');
+//var test = require('./config/keys');
 
 //app is the Express object that listens to the specifies port and "/" specifies the route"
 //This method will watch for requests accessing "/"
@@ -19,35 +19,35 @@ app.get('/', (req, res) => res.send("Hello Damini !!"));
 //callback URL is the redirect URI which is registered at console.developer.google.com and users get
 // redirected to this page after google grant the permission to user
 //Passport object uses google strategy object whic is imported at the top of the page
-passport.use(new GoogleStrategy(
-  {
-  clientID: test.Googleclientid,
-  clientSecret: test.Googlesecretclientid,
-  callbackURL: "/auth/google/callback"
-},
-   (accessToken, refreshToken, profile, done) => {
-    console.log(accessToken);
-    console.log("refresh " + refreshToken);
-    console.log("profile" + JSON.stringify(profile));
-  }
- )
-);
+// passport.use(new GoogleStrategy(
+//   {
+//   clientID: test.Googleclientid,
+//   clientSecret: test.Googlesecretclientid,
+//   callbackURL: "/auth/google/callback"
+// },
+//    (accessToken, refreshToken, profile, done) => {
+//     console.log(accessToken);
+//     console.log("refresh " + refreshToken);
+//     console.log("profile" + JSON.stringify(profile));
+//   }
+//  )
+// );
+// //
+// // when we use the string 'google' , we are internally using the above defined  GoogleStrategy object
+// //Google has a list of various other scopes as well like read all contact images, photos inside drive
+// //when we access this url it gets redirected to google auth web page and uses above passport object for Authentication
+// app.get('/auth/google', passport.authenticate('google', { scope: ['profile','email'] }));
 //
-// when we use the string 'google' , we are internally using the above defined  GoogleStrategy object
-//Google has a list of various other scopes as well like read all contact images, photos inside drive
-//when we access this url it gets redirected to google auth web page and uses above passport object for Authentication
-app.get('/auth/google', passport.authenticate('google', { scope: ['profile','email'] }));
-
-//When user is redirected to the path "/auth/google/callback", there will be a "CODE" in the URL which is sent by Google server
-//passport will not try to authenticate the user for the first time but instead share the code back with the google servers
-//when google recieves the code it redirects to this page(auth/google/callback) with the user details mentioned in scope
-//At the second time, callback method of passport.use() is called.
-
-app.get('/auth/google/callback',
-  passport.authenticate('google', { failureRedirect: '/' }),
-  function(req, res) {
-    // Successful authentication, redirect home.
-    res.send("Successful");
-  });
+// //When user is redirected to the path "/auth/google/callback", there will be a "CODE" in the URL which is sent by Google server
+// //passport will not try to authenticate the user for the first time but instead share the code back with the google servers
+// //when google recieves the code it redirects to this page(auth/google/callback) with the user details mentioned in scope
+// //At the second time, callback method of passport.use() is called.
+//
+// app.get('/auth/google/callback',
+//   passport.authenticate('google', { failureRedirect: '/' }),
+//   function(req, res) {
+//     // Successful authentication, redirect home.
+//     res.send("Successful");
+//   });
 //Express app is instructing node to listen to port 500(refer diagram Node_vs_Express.jpg)
 app.listen(port);
