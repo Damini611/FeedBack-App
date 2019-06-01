62dea206c59e5bce902937d68a16f1b03456c6fa
Damini Singh <damini.singh@mavs.uta.edu>
2019-06-01 16:42:06 -0400

    Added environment variables

---
+++ Server/config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    Googleclientid :process.env.GOOGLECLIENTID ,
+    Googlesecretclientid : process.env.GOOGLESECRETCLIENTID,
+    mongoURI: process.env.MONGOURI,
+    cookieKey: process.env.COOKIEKEY
+};
